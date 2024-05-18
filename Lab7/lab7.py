3385bc78977bbbc67d75406d3d405722364bb675
YourDear <geraldineguevara777@gmail.com>
2024-05-18 15:23:30 +0300

    fixes

--- Lab7/lab7.py
+++ Lab7/lab7.py
@@ -6,18 +6,16 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation, PillowWriter
 
 # №1
-# Создаем массивы с случайными числами
+
 size = 1000000
 list1 = [np.random.random() for _ in range(size)]
 list2 = [np.random.random() for _ in range(size)]
 
-# Производим перемножение с использованием списков Python
 start_time = time.perf_counter()
 result_list = [a * b for a, b in zip(list1, list2)]
 end_time = time.perf_counter()
 print(f"Время выполнения с использованием списков: {end_time - start_time} секунд")
 
-# Производим перемножение с использованием NumPy
 array1 = np.array(list1)
 array2 = np.array(list2)
 
@@ -27,14 +25,10 @@ end_time = time.perf_counter()
 print(f"Время выполнения с использованием NumPy: {end_time - start_time} секунд")
 
 # №2
-# Загрузка данных
 data = pd.read_csv('data1.csv', encoding='cp1251', delimiter=";")
+x = data.iloc[:, 3]  
+y = data.iloc[:, 17] 
 
-# Выделение данных из столбцов 4 и 18
-x = data.iloc[:, 3]  # столбец 4
-y = data.iloc[:, 17] # столбец 18
-
-# Построение графиков
 plt.figure(figsize=(12, 6))
 plt.plot(x, label='Column 4')
 plt.plot(y, label='Column 18')
@@ -53,11 +47,9 @@ plt.ylabel('Column 18')
 plt.show()
 
 # №3
-# Параметры для графика
 x = np.linspace(-5 * np.pi, 5 * np.pi, 1000)
 y = np.cos(x)
 z = np.sin(x)
-
 # Построение трехмерного графика
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -69,34 +61,30 @@ ax.set_zlabel('Z (sin(x))')
 plt.show()
 
 #Доп Зад
-# Определение функции для построения
 def sin_wave(x):
     return np.sin(x)
 
-# Создание фигуры и осей
+
 fig, ax = plt.subplots()
 xdata, ydata = [], []
 ln, = plt.plot([], [], 'r-', animated=True)
 
-# Инициализация осей
 def init():
     ax.set_xlim(0, 2*np.pi)
     ax.set_ylim(-1, 1)
     return ln,
 
-# Функция обновления данных для анимации
+
 def update(frame):
     xdata.append(frame)
     ydata.append(sin_wave(frame))
     ln.set_data(xdata, ydata)
     return ln,
 
-# Создание анимации
+
 ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
                     init_func=init, blit=True)
 
-# Сохранение анимации в файл GIF с использованием Pillow
-ani.save('sin_wave_animation.gif', writer='pillow', fps=30)
 
-# Показать результат
+ani.save('sin_wave_animation.gif', writer='pillow', fps=30)
 plt.show()
